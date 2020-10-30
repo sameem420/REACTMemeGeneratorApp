6096cced7f2523dec4db4999af2392eb367a6fbe
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-10-30 17:21:18 +0500

    created react App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import MemeGenerator from './MemeGenerator';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hello REACT</h1>
+      <MemeGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MemeGenerator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Meme Generator</h1>
+            </div>
+        );
+    };
+}
+
+export default MemeGenerator;
