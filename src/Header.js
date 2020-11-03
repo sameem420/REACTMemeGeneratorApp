a176899b4490934f83d78dff1297fcd88c2b7d53
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-04 00:39:31 +0500

    ✔️ fixed styling issues in Header

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
             <img 
                 src="https://i.ibb.co/8mkZ00b/Trollface.png" 
                 alt="Troll Face"
-                width="150"
+                width="120"
             />
             <p>Meme Generator</p>
         </header>
