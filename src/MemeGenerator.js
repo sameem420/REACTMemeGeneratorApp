f52ea317fa42cf5f4a005bbd3039ae22513c5bf6
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-03 23:00:20 +0500

    ✔️ save/download Meme feature - DONE

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.css';
 import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 import FileSaver from 'file-saver';
 
 class MemeGenerator extends React.Component {
